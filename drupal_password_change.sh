0332b45258cf2514e6161f4c9d613f15baf4fcb1
Joey Meyer <joeymeyer1986@gmail.com>
2016-12-13 23:39:27 -0600

    added condition to see if mysql was running before running other scripts

--- drupal_password_change.sh
+++ drupal_password_change.sh
@@ -9,40 +9,44 @@ drupalAdmin="..."
 query="show databases;"
 
 # check if mysql is running and for how long.
-mysql -u root -e STATUS | grep -i uptime
+mysqlUp = mysql -u root -e STATUS | grep -i uptime
+if [$mysqlUp == runningTrue]
+then
+  # Loop and collect the databases in an array from mysql.
+  while read line
+  do
+    myarray+=($line)
+  done < <(mysql -u${user} -p${psw} -e "${query}" -B)
 
-# Loop and collect the databases in an array from mysql.
-while read line
-do
-  myarray+=($line)
-done < <(mysql -u${user} -p${psw} -e "${query}" -B)
+  # prompt the user for the password.
+  read -p "what would you like the new drupal password to be?" selectedPassword
 
-# prompt the user for the password.
-read -p "what would you like the new drupal password to be?" selectedPassword
+  # repeat the password back to the user.
+  echo "you have choosen, $selectedPassword"
 
-# repeat the password back to the user.
-echo "you have choosen, $selectedPassword"
+  # run the drupal hash script.
+  pushd /var/www/drupal/scripts
+  p= ./password-hash.sh $selectedPassword
+  popd
 
-# run the drupal hash script.
-pushd /var/www/drupal/scripts
-p= ./password-hash.sh $selectedPassword
-popd
+  for databaseArray in "myarray[@]"
+  do
+    # inform admin which database we are looking at out of the array.
+    echo "checking: " $databaseArray
 
-for databaseArray in "myarray[@]"
-do
-  # inform admin which database we are looking at out of the array.
-  echo "checking: " $databaseArray
+    # Store returned value to be evaluated.
+    usernameStatus = mysql -u${user} -p${psw} -D${databaseArray} -e "select name from users where name='${drupalAdmin}';"
 
-  # Store returned value to be evaluated.
-  usernameStatus = mysql -u${user} -p${psw} -D${databaseArray} -e "select name from users where name='${drupalAdmin}';"
-
-  # Check if specific user exist on the table
-  if [ $usernameStatus != null ]
-  then
-    # change the specific users password to the hashed password.
-    mysql -u root -Bse "USE $databaseArray; UPDATE users SET pass = $p WHERE name='sds_admin'; SELECT pass FROM users WHERE name='${drupalAdmin}';"
-  else
-    # inform admin that the specific database does not
-    echo $databaseArray " does not have a user named " $drupalAdmin
-  fi
-done
+    # Check if specific user exist on the table
+    if [ $usernameStatus != null ]
+    then
+      # change the specific users password to the hashed password.
+      mysql -u root -Bse "USE $databaseArray; UPDATE users SET pass = $p WHERE name='sds_admin'; SELECT pass FROM users WHERE name='${drupalAdmin}';"
+    else
+      # inform admin that the specific database does not
+      echo $databaseArray " does not have a user named " $drupalAdmin
+    fi
+  done
+else
+  echo "mysql isn't running"
+fi
